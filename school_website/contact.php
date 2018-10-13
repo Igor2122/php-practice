3fae543ef663ff6e6386d74b04e749cbd0df0d4a
Igor <igor_pavlenko@yahoo.com>
2018-10-13 19:50:48 +0300

    content edited on the pages

--- school_website/contact.php
+++ school_website/contact.php
@@ -21,38 +21,9 @@
     <h1>Обратная связь</h1>
     <!-- Заголовок -->
     <!-- Область основного контента -->
-    <h3>Адрес</h3>
-    <p>123456 Москва, Малый Американский переулок 21</p>
-    <h3>Задайте вопрос</h3>
-    <form action='' method='post'>
-      <label>Тема письма: </label>
-      <br />
-      <input name='subject' type='text' size="50" />
-      <br />
-      <label>Содержание: </label>
-      <br />
-      <textarea name='body' cols="50" rows="10"></textarea>
-      <br />
-      <br />
-      <input type='submit' value='Отправить' />
-    </form>
+
     <!-- Область основного контента -->
-  </div>
-  <div id="nav">
-    <h2>Навигация по сайту</h2>
-    <!-- Меню -->
-    <ul>
-      <li><a href='index.php'>Домой</a>
-      </li>
-      <li><a href='about.php'>О нас</a>
-      </li>
-      <li><a href='contact.php'>Контакты</a>
-      </li>
-      <li><a href='table.php'>Таблица умножения</a>
-      </li>
-      <li><a href='calc.php'>Калькулятор</a>
-      </li>
-    </ul>
+
     <!-- Меню -->
   </div>
   <div id="footer">
