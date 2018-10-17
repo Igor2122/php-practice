9db63876474d3bdd15e56b920ec4deb0a362ab9b
Igor <igor_pavlenko@yahoo.com>
2018-10-17 11:30:31 +0300

    sessions added

--- forms.php
+++ forms.php
@@ -8,18 +8,18 @@
 //   echo $_GET['name'];
 //   echo $_GET['lastname'];
 // }
-// if (isset($_POST['name'])) {
-//   echo $_POST['name'];
-//   echo $_POST['lastname'];
-// }
-  if (isset($_REQUEST['name'])) {
-  echo $_REQUEST['name'];
-  echo $_REQUEST['lastname'];
+if (isset($_POST['name'])) {
+  echo $_POST['name'];
+  echo $_POST['lastname'];
 }
+//   if (isset($_REQUEST['name'])) {
+//   echo $_REQUEST['name'];
+//   echo $_REQUEST['lastname'];
+// }
  ?>
 
 <div style="margin: 50px 400px ">
-	 <form method="REQUEST" action="forms.php">
+	 <form method="post" action="forms.php">
 	  First name:<br>
 	  <input type="text" name="name" value="Mickey">
 	  <br>
--- test.php
+++ test.php
@@ -1,10 +1,8 @@
 <?php 
 
-include_once 'cookies/cookies.php'; 
+// include_once 'cookies/cookies.php'; 
 // include_once 'headers/headers.php'; 
 // include_once 'headers/content-disposition.php'; 
 // include_once 'caching/caching.php'; 
 // include_once 'hash/hash.php'; 
-include_once 'sessions/sessions.php'; 
-include_once 'sessions/session_1.php'; 
-include_once 'sessions/session_2.php'; 
+// include_once 'sessions/session-1.php';
