91d21f96aba536133b0f95add617b6b99029e7cb
Igor <igor_pavlenko@yahoo.com>
2018-10-18 09:04:34 +0000

    added directory

--- test.php
+++ test.php
@@ -6,3 +6,20 @@
 // include_once 'caching/caching.php'; 
 // include_once 'hash/hash.php'; 
 // include_once 'sessions/session-1.php';
+
+
+// we can check what is in the folder
+$dir = opendir(".");
+
+while($name = readdir($dir)){
+    if($name == '.' or $name == '..'){
+        continue;
+    }
+    if(is_dir($name)){
+        echo '[' . $name . ' - this is folder' . ']<br>';
+    } else {
+        echo $name . '<br>';
+    }
+}
+
+closedir($dir);
