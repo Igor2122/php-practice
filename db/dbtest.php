8b6ec4fb44f96c0d0ada5cb9f45c80e1a53b5c84
Igor <igor_pavlenko@yahoo.com>
2018-10-19 18:12:14 +0300

    parsing the result of db query

--- db/dbtest.php
+++ db/dbtest.php
@@ -12,15 +12,38 @@ $link = mysqli_connect('localhost', 'root', 'root', 'web') or die ("Could not co
 	// 	echo 'hi there';
 }*/ 
 
-$res = mysqli_query($link, "SET NAME 'utf-8'");
-
+// $res = mysqli_query($link, "SET NAMES 'utf-8'");
+// check for DB errors 
 if(!$res){
 	echo mysqli_error($link);
 }
 
+$sql = "SELECT * FROM teachers";
+$res = mysqli_query($link, $sql);
+// echo $sql;// if there was an error in the reques we can directly see it in the output 
 
-
+echo mysqli_error($link);
+// fetch the data from the return object
 // change the db without interrupting the connceciton 
 mysqli_select_db($link, "test");
+	mysqli_close($link); 
+// after we got the result we can close the connection 
+	// we need to parse the result object to get the data out
 
-mysqli_close($link);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
